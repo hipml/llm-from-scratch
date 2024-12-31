0c9605f9137f4092bde10787a0d5f2ed55feb7c7
Paul Lambert <paul.lambert@linux.com>
2024-12-31 08:52:18 -0600

    UNK, EOS tokens added to simple tokenizer

--- tokenizer.py
+++ tokenizer.py
@@ -4,26 +4,30 @@ with open('the-verdict.txt', 'r', encoding='utf-8') as f:
     raw_text = f.read()
 preprocessed = re.split(r'([,.?_!"()\']|--|\s)', raw_text)
 preprocessed = [item for item in preprocessed if item.strip()]
-# 
+ 
 all_words = sorted(list(set(preprocessed)))
-# vocab_size = len(all_words)
-# print(vocab_size)
-# 
+all_words.extend(["<|endoftext|>", "<|unk|>"])
+
 vocab = {token:integer for integer,token in enumerate(all_words)}
-# for i, item in enumerate(vocab.items()):
+# print(len(vocab.items()))
+# for i, item in enumerate(list(vocab.items())[-5:]):
 #     print(item)
-#     if i > 50:
-#         break
 
 
-class SimpleTokenizerV1:
+class SimpleTokenizerV2:
     def __init__(self, vocab):
         self.str_to_int = vocab
         self.int_to_str = {i:s for s,i in vocab.items()}
 
     def encode(self, text):
         preprocessed = re.split(r'([,.?_!"()\']|--|\s)', text)
-        preprocessed = [item for item in preprocessed if item.strip()]
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+
+        preprocessed = [
+            item if item in self.str_to_int
+            else "<|unk|>" for item in preprocessed
+        ]
+
         ids = [self.str_to_int[s] for s in preprocessed]
         return ids
 
@@ -32,7 +36,11 @@ class SimpleTokenizerV1:
         text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
         return text
        
-tokenizer = SimpleTokenizerV1(vocab)
+tokenizer = SimpleTokenizerV2(vocab)
 text = """"It's the last he painted, you know," Mrs. Gisburn said with pardonable pride."""
 ids = tokenizer.encode(text)
 print(ids)
+print(tokenizer.decode(ids))
+
+text = "Hello, do you like tea?"
+tokenizer.encode(text)
