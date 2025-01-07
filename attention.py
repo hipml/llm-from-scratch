334208e1633ee58da40ba620c1c32f7bfb51c9c3
Paul Lambert <paul.lambert@linux.com>
2025-01-07 14:06:36 -0600

    Attention works

--- attention.py
+++ attention.py
@@ -169,3 +169,92 @@ context_length = batch.shape[1]
 ca = CausalAttention(d_in, d_out, context_length, 0.0)
 context_vecs = ca(batch)
 print(context_vecs.shape)
+
+class MultiHeadAttentionWrapper(nn.Module):
+    def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
+        super().__init__()
+        self.heads = nn.ModuleList(
+            [CausalAttention(d_in, d_out, context_length, dropout, qkv_bias)
+             for _ in range(num_heads)]
+        )
+
+        self.out_proj = nn.Linear(d_out * num_heads, d_out*num_heads)
+
+    def forward(self, x):
+        context_vec = torch.cat([head(x) for head in self.heads], dim=-1)
+        return self.out_proj(context_vec)
+
+torch.manual_seed(123)
+context_length = batch.shape[1] # number of tokens
+num_heads = 2
+d_in, d_out = 3, 1
+mha = MultiHeadAttentionWrapper(d_in, d_out, context_length, 0.0, num_heads, False)
+context_vecs = mha(batch)
+
+print(f'{context_vecs} | {context_vecs.shape}')
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
+        super().__init__()
+        assert(d_out % num_heads == 0), "d_out must be divisible by num_heads"
+
+        self.d_out = d_out
+        self.num_heads = num_heads
+
+        # reduce the projection dim to match the desired output dim
+        self.head_dim = d_out // num_heads
+
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+
+        # Linear layer to combine head outputs
+        self.out_proj = nn.Linear(d_out, d_out)         
+
+        self.dropout = nn.Dropout(dropout)
+        self.register_buffer('mask', torch.triu(torch.ones(context_length, context_length), diagonal=1))
+
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape
+
+        # shape: [b, num_tokens, d_out]
+        keys = self.W_key(x) 
+        values = self.W_value(x)
+        queries = self.W_query(x)
+
+        # we implicitly split the matrix by adding a 'num_heads' dimension
+        # unroll last dim: [b, num_tokens, d_out] -> [b, num_tokens, num_heads, head_dim]
+        keys = keys.view(b, num_tokens, self.num_heads, self.head_dim)
+        values = values.view(b, num_tokens, self.num_heads, self.head_dim)
+        queries = queries.view(b, num_tokens, self.num_heads, self.head_dim)
+
+        # transpose
+        # [b, num_tokens, num_heads, head_dim] -> [b, num_heads, num_tokens, head_dim]
+        keys = keys.transpose(1, 2)
+        values = values.transpose(1, 2)
+        queries = queries.transpose(1, 2)
+
+        # compute scaled dot-product attention (self-attention) with a causal mask
+        attn_scores = queries @ keys.transpose(2, 3) # dot product for each head
+
+        # original mask truncated to num of tokens and convert to bool
+        mask_bool = self.mask.bool()[:num_tokens, :num_tokens]
+
+        # use the mask to fill attention scores
+        attn_scores.masked_fill_(mask_bool, -torch.inf)
+
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+        attn_weights = self.dropout(attn_weights)
+
+        # [b, num_tokens, num_heads, head_dim]
+        context_vec = (attn_weights @ values).transpose(1, 2)
+
+        # combine heads where self.d_out = self.num_heads * self.head_dim
+        context_vec = context_vec.contiguous().view(b, num_tokens, self.d_out)
+        context_vec = self.out_proj(context_vec) # optional projection
+
+        return context_vec
+
+        
+        
