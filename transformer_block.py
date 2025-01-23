45d50df502f1dfb37430154ddddfc0c37feb7c50
Paul Lambert <paul.lambert@linux.com>
2025-01-23 10:09:22 -0600

    complete transformer block with mh dpa, feed forward, layer norm, skip connections, and dropout

---
+++ transformer_block.py
@@ -0,0 +1,52 @@
+from attention import MultiHeadAttention
+from activation import FeedForward
+from normalization import LayerNorm
+
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.att = MultiHeadAttention(
+            d_in = cfg['emb_dim'],
+            d_out = cfg['emb_dim'],
+            context_length = cfg['context_length'],
+            num_heads = cfg['n_heads'],
+            dropout = cfg['drop_rate'],
+            qkv_bias=cfg['qkv_bias']
+        )
+
+        self.ff = FeedForward(cfg)
+        self.norm1 = LayerNorm(cfg['emb_dim'])
+        self.norm2 = LayerNorm(cfg['emb_dim'])
+        self.drop_resid = nn.Dropout(cfg['drop_rate'])
+
+    def forward(self, x):
+        shortcut = x
+        x = self.norm1(x)
+        x = self.att(x)
+        x = self.drop_resid(x)
+        x = x + shortcut
+
+        shortcut = x
+        x = self.norm2(x)
+        x = self.ff(x)
+        x = self.drop_resid(x)
+        x = x + shortcut
+        return x
+            
+GPT_CONFIG_124M = {
+    "vocab_size": 50257, # Vocabulary size
+    "context_length": 1024, # Context length
+    "emb_dim": 768, # Embedding dimension
+    "n_heads": 12, # Number of attention heads
+    "n_layers": 12, # Number of layers
+    "drop_rate": 0.1, # Dropout rate
+    "qkv_bias": False # Query-Key-Value bias
+}
+
+torch.manual_seed(123)
+x = torch.rand(2, 4, 768)
+block = TransformerBlock(GPT_CONFIG_124M)
+output = block(x)
+
+print(f'Input shape: {x.shape}')
+print(f'Output shape: {output.shape}')
